ac5ca7468a01fa51db4375518cd1bf7ad32f51b7
gplesz <gabor.plesz@gmail.com>
2017-06-06 13:15:10 +0200

    #5: egy bujócska számláló

---
+++ 04Ciklus/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04Ciklus
+{
+    /// <summary>
+    /// Írjunk egy olyan programot, ami a bujócskában elszámol 10-ig
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("3");
+            Console.WriteLine("4");
+            Console.WriteLine("5");
+            Console.WriteLine("6");
+            Console.WriteLine("7");
+            Console.WriteLine("8");
+            Console.WriteLine("9");
+            Console.WriteLine("10");
+            Console.WriteLine();
+
+
+            //De mi van akkor, ha 100-ig kell eszámolni?
+            //vagy 10.000-ig?
+
+            //háromféle ciklus: while, for és foreach
+
+            //az "i" a ciklusváltozó
+
+            for (int i = 1; i <= 100; i++)  //i++ ugyanaz, mint ez: i = i + 1;
+            { //ez itt a ciklus törzse
+                System.Threading.Thread.Sleep(500); //A programunk vár fél másodpercet
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 04Ciklus/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("04Ciklus")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("04Ciklus")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f20c8d14-b3ce-46f2-9bcf-1d55a56f13f2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
