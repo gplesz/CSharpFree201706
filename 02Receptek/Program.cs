e670915a29d9218df444523de70d1566c085ab49
gplesz <gabor.plesz@gmail.com>
2017-06-06 11:01:20 +0200

    #2: Egy recept kiírása, és enterre várakozás

---
+++ 02Receptek/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02Receptek
+{
+    /// <summary>
+    /// Legyen az a feladat, hogy egy pörkölt recept hozzávalóit kiírjuk a képernyőre
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Marhahús");
+            Console.WriteLine("Vöröshagyma");
+            Console.WriteLine("Olaj");
+            Console.WriteLine("Pirospaprika");
+
+            Console.ReadLine();
+        }
+    }
+}
